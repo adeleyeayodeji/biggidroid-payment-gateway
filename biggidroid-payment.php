216ab62d0cb057440ef0d39fa282c35731a39dc3
adeleyeayodeji <biodunhi@gmail.com>
2024-02-19 09:56:30 +0100

    Add WooCommerce check and admin notice

--- biggidroid-payment.php
+++ biggidroid-payment.php
@@ -22,17 +22,31 @@ define('BIGGI_DROID_PAYMENT_VERSION', '0.1.0');
 define('BIGGI_DROID_PAYMENT_PLUGIN_URL', plugin_dir_url(__FILE__));
 define('BIGGI_DROID_PAYMENT_PLUGIN_PATH', plugin_dir_path(__FILE__));
 
-//add action plugins loaded
-add_action('plugins_loaded', 'biggidroid_payment_init');
+//check if WooCommerce is active
+if (!in_array('woocommerce/woocommerce.php', apply_filters('active_plugins', get_option('active_plugins')))) {
+    //add notice
+    add_action('admin_notices', 'biggidroid_payment_woocommerce_notice');
+} else {
+    //add action plugins loaded
+    add_action('plugins_loaded', 'biggidroid_payment_init');
+}
+
 
 //initialize the plugin
 function biggidroid_payment_init()
 {
-    //check if WooCommerce is active
-    if (in_array('woocommerce/woocommerce.php', apply_filters('active_plugins', get_option('active_plugins')))) {
-        //check if the class exists Biggi_Droid_Payment_Gateway
-        if (!class_exists('Biggi_Droid_Payment_Gateway')) {
-            include_once BIGGI_DROID_PAYMENT_PLUGIN_PATH . '/includes/main-file.php';
-        }
+    //check if the class exists Biggi_Droid_Payment_Gateway
+    if (!class_exists('Biggi_Droid_Payment_Gateway')) {
+        include_once BIGGI_DROID_PAYMENT_PLUGIN_PATH . '/includes/main-file.php';
     }
+}
+
+//biggidroid_payment_woocommerce_notice
+function biggidroid_payment_woocommerce_notice()
+{
+    ob_start();
+    //require the admin notice template
+    require_once BIGGI_DROID_PAYMENT_PLUGIN_PATH . '/templates/admin_notice.php';
+    $html = ob_get_clean();
+    echo $html;
 }
---
+++ templates/admin_notice.php
@@ -0,0 +1,9 @@
+<?php
+//security
+if (!defined('ABSPATH')) {
+    exit('You must not access this file directly');
+}
+?>
+<div class="notice notice-error is-dismissible">
+    <p><?php _e('BiggiDroid Payment Gateway requires WooCommerce to be installed and active.', 'biggidroid-payment'); ?></p>
+</div>
